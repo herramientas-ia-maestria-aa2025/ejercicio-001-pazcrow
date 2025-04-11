6a6385f6f9acb1557229ff4c354334a3d41bd82a
Luis Pazmiño <pazcrow19@hotmail.com>
2025-04-10 22:29:22 -0500

    Actualización de código

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
 with open('informacion.txt','r') as archivo:
-    datos = archivo.read()
-    datos_separados = datos.strip().split(';')
-    print(datos)
+    for linea in archivo:
+        datos = linea.strip().split(';')
+        print(f"*Nombres*: {datos[0]} *Apellidos*: {datos[1]} *Direccion*: {datos[2]} *Correo*: {datos[3]} ")
