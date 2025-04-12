b51d3c33da7eefec14be0664d558d5d6d9a7d37c
Luis Pazmiño <pazcrow19@hotmail.com>
2025-04-11 19:32:46 -0500

    Actualización

--- init.py
+++ init.py
@@ -1,4 +1,5 @@
 with open('informacion.txt','r') as archivo:
     for linea in archivo:
         datos = linea.strip().split(';')
-        print(f"*Nombres*: {datos[0]} *Apellidos*: {datos[1]} *Direccion*: {datos[2]} *Correo*: {datos[3]} ")
+        if(datos[1][0] == 'B'):
+            print(f"*Nombres*: {datos[0]} *Apellidos*: {datos[1]} *Direccion*: {datos[2]} *Correo*: {datos[3]} ")
